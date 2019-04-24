26af2effd94a089e3e4873ed6ce256dd8fd823ab
Stefan Kuhnert <stefan.kuhnert94@gmail.com>
2019-04-25 00:31:02 +0200

    DB design

---
+++ routes/register.php
@@ -0,0 +1,80 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+
+// TODO: Put exception in source
+class DecodeException extends Exception { }
+class LoginException extends Exception { }
+class TokenInvalidException extends Exception { }
+
+/* ***********************************************
+ * User API Group
+ * ***********************************************/
+$app->group('/register', function () use ($app)
+{
+   $app->post('/basic', function (Request $request, Response $response)
+   {
+      try 
+      {
+         $cred = $request->getParsedBody(); 
+
+         if($cred == NULL)
+            throw new DecodeException("Could not decode Body");
+         
+         if(!(isset($cred["username"]) && isset($cred["password"]) && $cred["username"] != "" && $cred["password"] != ""))
+         {
+            throw new LoginException("Could not retrieve Login Parameters");
+         }
+
+      } 
+      catch (Exception $e) 
+      {
+         return $response
+            ->withStatus(401)
+            ->withJson(["message" => $e->getMessage()]);
+      }
+
+      return $response
+         ->withStatus(200)
+         ->withJson($cred);
+   });
+
+   $app->post('/gauth', function (Request $request, Response $response)
+   {
+      try 
+      {
+         $cred = $request->getParsedBody(); 
+
+         if($cred == NULL)
+            throw new DecodeException("Could not decode Body");
+         
+         if(!(isset($cred["token"]) && $cred["token"] != "" ))
+         {
+            throw new LoginException("Could not retrieve Login Parameters");
+         }
+
+         $client = new Google_Client(['client_id' => Config::$gauth_secret]);  // Specify the CLIENT_ID of the app that accesses the backend
+         $payload = $client->verifyIdToken($cred["token"]);
+         
+         if ($payload) 
+         {
+            $userid = $payload['sub'];
+         } else {
+         }
+      } 
+      catch (Exception $e) 
+      {
+         return $response
+            ->withStatus(401)
+            ->withJson(["message" => $e->getMessage()]);
+      }
+
+      return $response
+         ->withJson($payload)
+         ->withStatus(200);
+   });
+});
+
+?>
--- structure/dndoodle.sql
+++ structure/dndoodle.sql
@@ -14,5 +14,55 @@ CREATE TABLE dnd_user
     user_name   VARCHAR(32) NOT NULL,
     user_gid    VARCHAR(64),
     user_pass   BINARY(60)
-    user_email  VARCHAR(128),
+    user_email  VARCHAR(128)
 );
+
+CREATE TABLE dnd_char
+(
+    char_id     INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    char_name   VARCHAR(64) NOT NULL,
+    last_edit   INT UNSIGNED DEFAULT 0,
+    user_id     INT UNSIGNED,
+    FOREIGN KEY (user_id) REFERENCES dnd_user(user_id)
+)
+
+CREATE TABLE dnd_char_attr
+(
+    attr_STR    TINYINT UNSIGNED NOT NULL,
+    attr_DEX    TINYINT UNSIGNED NOT NULL,
+    attr_CON    TINYINT UNSIGNED NOT NULL,
+    attr_INT    TINYINT UNSIGNED NOT NULL,
+    attr_WIS    TINYINT UNSIGNED NOT NULL,
+    attr_CHA    TINYINT UNSIGNED NOT NULL,
+    char_id     INT UNSIGNED,
+    FOREIGN KEY (char_id) REFERENCES dnd_char(char_id)
+)
+
+CREATE TABLE dnd_char_skills
+(
+    skill_ACRO TINYINT NOT NULL,
+    {"KNOW": "PROF", "MODI": "-1"}
+    skill_ANHA TINYINT NOT NULL,
+    skill_ARCA TINYINT NOT NULL,
+    skill_ATHL TINYINT NOT NULL,
+    skill_DECE TINYINT NOT NULL,
+    skill_HIST TINYINT NOT NULL,
+    skill_INSI TINYINT NOT NULL,
+    skill_INTI TINYINT NOT NULL,
+    skill_INVE TINYINT NOT NULL,
+    skill_MEDI TINYINT NOT NULL,
+    skill_NATU TINYINT NOT NULL,
+    skill_PERC TINYINT NOT NULL,
+    skill_PERF TINYINT NOT NULL,
+    skill_PERS TINYINT NOT NULL,
+    skill_RELI TINYINT NOT NULL,
+    skill_SLHA TINYINT NOT NULL,
+    skill_STEA TINYINT NOT NULL,
+    skill_SURV TINYINT NOT NULL,
+    char_id     INT UNSIGNED,
+    FOREIGN KEY (char_id) REFERENCES dnd_char(char_id)
+)
+
+CREATE TABLE dnd_char_saves
+(
+)
